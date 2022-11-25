7b302e7eb455192d024f04117813846bbd31fe7f
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-25 16:44:22 -0300

    Eliminé las variables seguidos y seguidores de la clase Perfil

--- Recuperatorio-PPII-2022/src/main/java/recuperatorio/Perfil.java
+++ Recuperatorio-PPII-2022/src/main/java/recuperatorio/Perfil.java
@@ -1,5 +1,44 @@
 package main.java.recuperatorio;
 
+
+
 public class Perfil {
+
+  
+    private String nickname;
+    private String nombre;
+    private String apellido;
+    private String descripcion;
     
+    
+
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+
 }
