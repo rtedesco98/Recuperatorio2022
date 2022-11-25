9d621d7522feb0da28b66e976122f00c4c3b2097
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-25 18:01:21 -0300

    creando test para seguir otro usuario

--- RedSocial-PPII-2022/src/tests/java/recuperatorio/App.java
+++ RedSocial-PPII-2022/src/tests/java/recuperatorio/App.java
@@ -1,8 +1,20 @@
 package tests.java.recuperatorio;
+import main.java.recuperatorio.*;
+
+import org.junit.*;
 
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+   @Test
+   public void debeSeguirUsuario(){
+    Perfil miPerfil = new Perfil ();
+    Usuario usuario = new Usuario ();
+    miPerfil.debeSeguirUsuario(usuario);
+
+    assert
+
+
+   }
+
+
 }
