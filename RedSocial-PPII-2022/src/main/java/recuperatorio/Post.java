d12ad29d2925c35a60d0ad0b33e98fb124bba402
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-28 00:24:37 -0300

    Clase post, está sin terminar (Subo YO forma  parte de Julian)

---
+++ RedSocial-PPII-2022/src/main/java/recuperatorio/Post.java
@@ -0,0 +1,32 @@
+package main.java.recuperatorio;
+
+public class Post {
+    private String descripcion;
+    //private Object quitarMeGusta;
+ 
+    public String descripcion() {
+     return descripcion;
+ }
+ public void setdescripcion(String descripcion) {
+     this.descripcion = descripcion;
+ }
+ 
+ public void quitarMeGusta(){
+ //Object quitarMeGusta;
+ this.quitarMeGusta = quitarMeGusta;
+ 
+ }
+ 
+ public void meGusta(){
+ 
+ }
+ 
+ public void agregarEtiqueta(){
+ 
+ 
+ }
+ public void comentarPublicacion(){
+ 
+ 
+ }
+}
