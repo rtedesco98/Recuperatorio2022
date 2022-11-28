20e2b67db009ea69e7a22ba6ba765aebb97062b9
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-28 16:52:45 -0300

    clase etiqueta fin

--- RedSocial-PPII-2022/src/main/java/recuperatorio/Etiqueta.java
+++ RedSocial-PPII-2022/src/main/java/recuperatorio/Etiqueta.java
@@ -3,9 +3,22 @@ package main.java.recuperatorio;
 public class Etiqueta {
     private String nickname;
 
-    public Etiqueta (String nickname);
+    public Etiqueta (String nickname){
 
+        this.nickname = nickname;
+
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
     
 
-
+    public String soString(){
+        return "Etiqueta: " + this.nickname;
+    }
 }
