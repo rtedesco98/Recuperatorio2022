0636ff71e0bce3cc8a148bf53d00f2f48978f9bb
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-28 15:24:31 -0300

    se sube parte de post mas completo

--- RedSocial-PPII-2022/src/main/java/recuperatorio/Post.java
+++ RedSocial-PPII-2022/src/main/java/recuperatorio/Post.java
@@ -2,31 +2,39 @@ package main.java.recuperatorio;
 
 public class Post {
     private String descripcion;
+
     //private Object quitarMeGusta;
  
     public String descripcion() {
+ 
      return descripcion;
  }
  public void setdescripcion(String descripcion) {
+ 
      this.descripcion = descripcion;
  }
- 
  public void quitarMeGusta(){
+ 
  //Object quitarMeGusta;
- this.quitarMeGusta = quitarMeGusta;
+ 
+ this.quitarMeGusta = quitarMeGusta();
  
  }
  
  public void meGusta(){
  
+ this.meGusta = meGusta();
+ 
  }
  
  public void agregarEtiqueta(){
  
+ this.agregarEtiqueta() = agregarEtiqueta();
  
  }
  public void comentarPublicacion(){
- 
+     
+ this.comentarPublicacion();  comentarPublicacion();
  
  }
 }
