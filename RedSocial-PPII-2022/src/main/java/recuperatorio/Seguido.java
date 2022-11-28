d48befae5657bf3e442f1dc5d34196641f387e92
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-28 18:26:29 -0300

    Seguido

--- RedSocial-PPII-2022/src/main/java/recuperatorio/Seguido.java
+++ RedSocial-PPII-2022/src/main/java/recuperatorio/Seguido.java
@@ -1,5 +1,19 @@
 package main.java.recuperatorio;
 
 public class Seguido {
-    
+    private String nickname;
+
+    public Seguido(String nickname){
+        this.setNickname(nickname);
+
+    }
+
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
