bfd78082915c09b9e6d8be62e1e90ef6200bd2c9
rtedesco98 <rtedescosistemas@gmail.com>
2022-11-30 00:30:37 -0300

    comentario, archivo base

--- RedSocial-PPII-2022/src/main/java/recuperatorio/Post.java
+++ RedSocial-PPII-2022/src/main/java/recuperatorio/Post.java
@@ -2,7 +2,7 @@ package main.java.recuperatorio;
 
 public class Post {
     private String descripcion;
-
+    
     public String descripcion() {
  
      return descripcion;
@@ -11,30 +11,5 @@ public class Post {
     public void setdescripcion(String descripcion) {
  
      this.descripcion = descripcion;
-    }
-
-
-    public void quitarMeGusta(Megusta megusta){
-        
-    this.megusta.remove(megusta);
- 
-    }
- 
-    public void meGusta(){
- 
-    this.meGusta = meGusta();
- 
-    }
- 
-    public void agregarEtiqueta(){
- 
-    this.agregarEtiqueta() = agregarEtiqueta();
- 
-    }
-
-    public void comentarPublicacion(){
-     
-    this.comentarPublicacion();  comentarPublicacion();
- 
     }
 }
--- RedSocial-PPII-2022/src/tests/java/recuperatorio/App.java
+++ RedSocial-PPII-2022/src/tests/java/recuperatorio/App.java
@@ -16,6 +16,7 @@ public class App {
     */
    @Test
     public void debeSeguirUsuario(){
+      
     Usuario miUsuario = new Usuario (null, null, 0, 0, 0);
     miUsuario.SeguirUsuario(miUsuario);
     assertTrue(miUsuario.getSeguidos() == 1);
